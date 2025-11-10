d06cd0f96caa81c05595d75447f440d75ea85ab3
khushi-bharade18 <khushibharade@gmail.com>
2025-11-10 23:59:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+let inputBox = document.querySelector("#inputId");
+let buttons = document.querySelectorAll(".button");
+let str = "";
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    switch (true) {
+      // You clicked equals button.
+      case e.target.classList.contains("equal"):
+        if (str === "") {
+          inputBox.value = "Error";
+          str = "";
+        } else {
+          str = eval(str);
+          inputBox.value = str;
+          str = "";
+        }
+        break;
+      // You clicked plus button.
+      case e.target.classList.contains("plus"):
+        str += "+";
+        inputBox.value = str;
+        break;
+      // You clicked minus button.
+      case e.target.classList.contains("minus"):
+        str += "-";
+        inputBox.value = str;
+        break;
+      // You clicked multiply button.
+      case e.target.classList.contains("multiply"):
+        str += "*";
+        inputBox.value = str;
+        break;
+      // You clicked divide button.
+      case e.target.classList.contains("divide"):
+        str += "/";
+        inputBox.value = str;
+        break;
+      // You clicked power button.
+      case e.target.classList.contains("power"):
+        str += "**";
+        inputBox.value = str;
+        break;
+      // You clicked sqrt button.
+      case e.target.classList.contains("sqrt"):
+        str = eval(str);
+        // Converting string into floating point number
+        let num = parseFloat(str);
+        if (isNaN(num) || num < 0) {
+          inputBox.value = "Error : (-ve)";
+          str = "";
+          return;
+        }
+        // Find the square root of number
+        let sqrtNum = Math.sqrt(num);
+        str = formatNumber(sqrtNum);
+        function formatNumber(n) {
+          if (!isFinite(n)) return String(n);
+          if (Number.isInteger(n)) return String(n);
+          return parseFloat(n.toFixed(5)).toString();
+        }
+        inputBox.value = str;
+        str = "";
+        break;
+      // You clicked plus-miinus button.
+      case e.target.classList.contains("plus-minus"):
+        let regex = /(\(-?\d+(\.\d+)?\)|\d+(\.\d+)?)$/;
+        str = str.replace(regex, (match) => {
+          if (match.startsWith("(-")) {
+            return match.slice(2, -1);
+          } else {
+            return `(-${match})`;
+          }
+        });
+        inputBox.value = str;
+        break;
+      // You clicked delete button.
+      case e.target.classList.contains("delete"):
+        str = str.substring(0, str.length - 1);
+        inputBox.value = str;
+        break;
+      // You clicked all-clear button.
+      case e.target.classList.contains("all-clear"):
+        str = "";
+        inputBox.value = str;
+        break;
+      // Add numbers in string
+      default:
+        str += e.target.innerHTML;
+        inputBox.value = str;
+        break;
+    }
+  });
+});
