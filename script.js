7ac6fd5c90e09cf60b54bf9632b0a0119ea46e45
khushi-bharade18 <khushibharade@gmail.com>
2025-11-11 11:18:12 +0530

    Fixed Floating Point Numbers

--- script.js
+++ script.js
@@ -5,42 +5,56 @@ buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     switch (true) {
       // You clicked equals button.
+
       case e.target.classList.contains("equal"):
         if (str === "") {
           inputBox.value = "Error";
           str = "";
         } else {
-          str = eval(str);
+          let formatStr = eval(str);
+          str = formatNumber(formatStr);
           inputBox.value = str;
           str = "";
         }
         break;
+
       // You clicked plus button.
+
       case e.target.classList.contains("plus"):
         str += "+";
         inputBox.value = str;
         break;
+
       // You clicked minus button.
+
       case e.target.classList.contains("minus"):
         str += "-";
         inputBox.value = str;
         break;
+
       // You clicked multiply button.
+
       case e.target.classList.contains("multiply"):
         str += "*";
         inputBox.value = str;
         break;
+
       // You clicked divide button.
+
       case e.target.classList.contains("divide"):
         str += "/";
         inputBox.value = str;
         break;
+
       // You clicked power button.
+
       case e.target.classList.contains("power"):
         str += "**";
         inputBox.value = str;
         break;
+
       // You clicked sqrt button.
+
       case e.target.classList.contains("sqrt"):
         str = eval(str);
         // Converting string into floating point number
@@ -53,15 +67,12 @@ buttons.forEach((btn) => {
         // Find the square root of number
         let sqrtNum = Math.sqrt(num);
         str = formatNumber(sqrtNum);
-        function formatNumber(n) {
-          if (!isFinite(n)) return String(n);
-          if (Number.isInteger(n)) return String(n);
-          return parseFloat(n.toFixed(5)).toString();
-        }
         inputBox.value = str;
         str = "";
         break;
+
       // You clicked plus-miinus button.
+
       case e.target.classList.contains("plus-minus"):
         let regex = /(\(-?\d+(\.\d+)?\)|\d+(\.\d+)?)$/;
         str = str.replace(regex, (match) => {
@@ -73,17 +84,23 @@ buttons.forEach((btn) => {
         });
         inputBox.value = str;
         break;
+
       // You clicked delete button.
+
       case e.target.classList.contains("delete"):
         str = str.substring(0, str.length - 1);
         inputBox.value = str;
         break;
+
       // You clicked all-clear button.
+
       case e.target.classList.contains("all-clear"):
         str = "";
         inputBox.value = str;
         break;
+
       // Add numbers in string
+
       default:
         str += e.target.innerHTML;
         inputBox.value = str;
@@ -91,3 +108,9 @@ buttons.forEach((btn) => {
     }
   });
 });
+
+function formatNumber(n) {
+  if (!isFinite(n)) return String(n);
+  if (Number.isInteger(n)) return String(n);
+  return parseFloat(n.toFixed(5)).toString();
+}
